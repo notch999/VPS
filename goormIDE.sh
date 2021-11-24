9fe2b1f78247e2a7021eba20c98872354c4db801
Blog HAX <76058709+noct99@users.noreply.github.com>
2021-11-25 00:39:36 +0700

    Create goormIDE.sh

---
+++ goormIDE.sh
@@ -0,0 +1,26 @@
+sudo adduser admin --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "admin:admin" | sudo chpasswd
+sudo usermod -aG sudo,adm admin
+apt update
+clear
+echo "===================================="
+echo "Install XFCE"
+echo "Choose Keyboard 31 then 1"
+echo "===================================="
+sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+echo "=======25%"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "=============50%"
+sudo apt-get install -y xrdp > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal > /dev/null 2>&1
+echo "=================70%"
+echo xfce4-session >~/.xsession
+echo "=====================90%"
+sudo service xrdp start
+clear
+echo "=======================100%"
+echo "===================================="
+echo "Username : admin"
+echo "Password : admin"
+echo Go to settings add port 3389
+echo "===================================="
